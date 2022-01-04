71602ee9d24b0848be42ce6514f0a6188e7b897a
Thomas Crosby <thomascrosby3@gmail.com>
2022-01-04 16:03:25 +0000

    it gets api stuff and lists them!

--- src/index.js
+++ src/index.js
@@ -1,41 +1,89 @@
 import React from "react";
+import axios from "axios";
 import ReactDOM from "react-dom";
 import "./index.css";
 
+const baseURL = "http://18.170.32.252:8080/cocktails/all";
 
 //Entire page Div
-class Page extends React.Component {
-  renderTitle() {
-    return <Title />;
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      error: null,
+      isLoaded: false,
+      items: []
+    };
   }
 
-  renderTable() {
-    return <Table />;
+  componentDidMount() {
+    fetch("http://13.40.33.55:8080/cocktails/all")
+      .then(res => res.json())
+      .then(
+        (result) => {
+          this.setState({
+            isLoaded: true,
+            items: result
+          });
+        },
+        // Note: it's important to handle errors here
+        // instead of a catch() block so that we don't swallow
+        // exceptions from actual bugs in components.
+        (error) => {
+          this.setState({
+            isLoaded: true,
+            error
+          });
+        }
+      )
   }
 
-  render() { //Renders the components created
-    return (
-        <div>
-          <div>
-            {this.renderTitle()}
-          </div>
-          <div>
-            {this.renderTable()}
-          </div>
-        </div>
+  render() {
+    console.log("hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+    const { error, isLoaded, items } = this.state;
+    if (error) {
+      return <div>Error: {error.message}</div>;
+    } else if (!isLoaded) {
+      return <div>Loading...</div>;
+    } else {
+      return (
+        
+        <ul>
+          {items.map(item => (
+            <li key={item.id}>
+              {item.name} {item.cocktail_Instructions_number_of_instructions}
+            </li>
+          ))}
+        </ul>
       );
+    }
   }
 }
 
+
 ////Table
+class Table extends React.Component {
+  constructor(props) {
+     super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
+     this.state = { //state is by default an object
+        
+     }
+  }
+ render() { //Renders the components created
+  return (
+    <div>
+          <h1 id='title'>React Dynamic Table</h1>
+       </div>
+  )
+}
+}
+
+export default Table
+
+////Title
 //This is a function component instead of a react component
-class Table extends React.Component 
-{
-    render() {
-        return (
-            <div>What's good im a table</div>
-        );
-      }
+function Title(props) {
+  return <h1 className="title">Title</h1>;
 }
 
 
@@ -48,11 +96,7 @@ function Square(props) {
   );
 }
 
-////Title
-//This is a function component instead of a react component
-function Title(props) {
-  return <h1>Title</h1>;
-}
+
 
 //Parent of square so it can lift up the state
 class Board extends React.Component {
@@ -124,4 +168,4 @@ class Game extends React.Component {
 
 // ========================================
 //render the game into the root div
-ReactDOM.render(<Page />, document.getElementById("root"));
+ReactDOM.render(<MyComponent/>, document.getElementById("root"));
