8d8a9b302a922c09af5e5aa554389408c88b1e10
Thomas Crosby <thomascrosby3@gmail.com>
2022-01-06 09:56:47 +0000

    trying out github pages

--- src/index.js
+++ src/index.js
@@ -3,10 +3,36 @@ import axios from "axios";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-const baseURL = "http://18.170.32.252:8080/cocktails/all";
+const baseURL = "http://13.40.33.55:8080/cocktails";
 
 //Entire page Div
-class MyComponent extends React.Component {
+class Page extends React.Component {
+  render()
+  {
+    return (
+      <div>
+        <div>
+          {Title()}
+        </div>
+        <div>
+        <Table/>
+        </div>
+        <div>
+        <h1>Add Drink</h1>
+        <Form/>
+        </div>
+      </div>
+    );
+  }
+}
+
+////Title
+function Title(props) {
+  return <h1 className="title">Cocktail Database</h1>;
+}
+
+////Table
+class Table extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -17,7 +43,7 @@ class MyComponent extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://13.40.33.55:8080/cocktails/all")
+    fetch(baseURL + "/all")
       .then(res => res.json())
       .then(
         (result) => {
@@ -26,9 +52,7 @@ class MyComponent extends React.Component {
             items: result
           });
         },
-        // Note: it's important to handle errors here
-        // instead of a catch() block so that we don't swallow
-        // exceptions from actual bugs in components.
+
         (error) => {
           this.setState({
             isLoaded: true,
@@ -39,7 +63,6 @@ class MyComponent extends React.Component {
   }
 
   render() {
-    console.log("hello!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
     const { error, isLoaded, items } = this.state;
     if (error) {
       return <div>Error: {error.message}</div>;
@@ -47,11 +70,11 @@ class MyComponent extends React.Component {
       return <div>Loading...</div>;
     } else {
       return (
-        
+        //TODO: UPDATE THIS
         <ul>
           {items.map(item => (
-            <li key={item.id}>
-              {item.name} {item.cocktail_Instructions_number_of_instructions}
+            <li key={item.name}>
+              Cocktail: {item.name} Number of Instructions: {item.cocktail_Instructions_number_of_instructions}
             </li>
           ))}
         </ul>
@@ -60,32 +83,60 @@ class MyComponent extends React.Component {
   }
 }
 
-
-////Table
-class Table extends React.Component {
+class Form extends React.Component {
   constructor(props) {
-     super(props) //since we are extending class Table so we have to use super in order to override Component class constructor
-     this.state = { //state is by default an object
-        
-     }
+    super(props);
+    this.state = {
+      drink: '1', 
+      instr: '2'
+    };
+    this.handleDrinkChange = this.handleDrinkChange.bind(this);
+    this.handleInstructionChange = this.handleInstructionChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
- render() { //Renders the components created
-  return (
-    <div>
-          <h1 id='title'>React Dynamic Table</h1>
-       </div>
-  )
-}
-}
 
-export default Table
+  handleDrinkChange(event) {
+    this.setState({
+      drink: event.target.value
+    });
+  }
 
-////Title
-//This is a function component instead of a react component
-function Title(props) {
-  return <h1 className="title">Title</h1>;
+  handleInstructionChange(event) {
+    this.setState({
+      instr: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    alert('Cocktail added: ' + this.state.drink +" + "+ this.state.instr);
+    event.preventDefault();
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ title: 'React POST Request Example' })
+  };
+  fetch(baseURL + "/add/cocktail?name=" + this.state.drink + "&Cocktail_Instructions_number_of_instructions=" + this.state.instr , requestOptions);
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+<input
+          inputType = {"text"}
+            value={this.state.drink}
+            onChange={this.handleDrinkChange} />
+
+<input
+          inputType = {"text"}
+            value={this.state.instr}
+            onChange={this.handleInstructionChange} />
+
+        <input type="submit" value="Add Drink" />
+      </form>
+    );
+  }
 }
-
 
 //This is a function component instead of a react component (use for render only)
 function Square(props) {
@@ -95,9 +146,6 @@ function Square(props) {
     </button>
   );
 }
-
-
-
 //Parent of square so it can lift up the state
 class Board extends React.Component {
     //prop can be passed from the parent to the child of the class
@@ -168,4 +216,4 @@ class Game extends React.Component {
 
 // ========================================
 //render the game into the root div
-ReactDOM.render(<MyComponent/>, document.getElementById("root"));
+ReactDOM.render(<Page/>, document.getElementById("root"));
